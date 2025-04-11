1db3a71799b54b8b183ebed083758ccac7d053c7
Ankita Goyal <goyalankita0325@gmail.com>
2025-04-11 18:56:56 +0530

    working on auth

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,33 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+interface AuthLayoutprops {
+    children : React.ReactNode;
+}
+const AuthLayout  = ({children} : AuthLayoutprops) => {
+    return (
+        <main className="bg-neutral-100 min-h-screen">
+            <div className="mx-auto max-w-screen-2xl p-4">
+                <nav className="flex justify-between items-center">
+                   <Image src="/logo.svg" height={50} width={100} alt="Logo"/>
+                   <Button variant="secondary">
+                    Sign-up
+                   </Button>
+                </nav>
+                <div className="flex flex-col items-center justify-center pt-4">
+                {children}
+                </div>
+            </div>
+        </main>
+    )
+}
+
+export default AuthLayout;
+
+//This interface AuthLayoutprops is being used to define the shape of props that the AuthLayout component can accept. Let's break it down:
+
+//1.children: This is a special prop in React that represents any content that will be nested inside this component
+
+//2.React.ReactNode: This is a TypeScript type that can represent any content that React can render (like text, elements, components, etc.)
+
+//This is a core concept in Next.js and React where layouts act as wrappers that can contain different page content while maintaining a consistent structure.
---
+++ src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,8 @@
+import SignInCard from "@/app/features/auth/components/sign-in-card";
+const SignInPage  = () => {
+    return (
+       <SignInCard/>
+    )
+};
+
+export default SignInPage; //Next.js requires pages to have a default export 
